04fa36d11c627df2cbb6b85e84d5e9fc11e7f313
khalillan <landoulsi.khalil@esprit.tn>
2022-05-07 01:59:39 +0100

    add

--- pidev/src/app/dashboard-admin/header-admin/header-admin.component.ts
+++ pidev/src/app/dashboard-admin/header-admin/header-admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/model/user';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-header-admin',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-admin.component.css']
 })
 export class HeaderAdminComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+ 
+  constructor(private auth:AuthService,private router:Router) { }
+  onLogout(){
+    this.auth.logout();
   }
+ 
+  ngOnInit(): void {
+    this.auth.loadToken();
+    if (this.auth.getToken()==null || 
+        this.auth.isTokenExpired())
+          this.router.navigate(['/user-login']);
+  }
+
+
+
+ 
 
 }
